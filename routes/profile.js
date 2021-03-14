4ee972c80f3d9f84e464b224b620088737478113
Yufei Zhou <yuz051@ucsd.edu>
2021-03-13 17:09:43 -0800

    style

--- routes/profile.js
+++ routes/profile.js
@@ -1,23 +1,5 @@
-var profiles = require('../projects.json')['projects'];
+var likes = require('../like.json');
 
-exports.profileInfo = function(request, response) {
-	var profileID = request.params.id;
-	var profile = getProfileData(profileID);
-  	response.json(profile);
-}
-
-exports.view = function(request, response) {
-	var profileID = request.params.id;
-	var profile = getProfileData(profileID);
-	response.render('profile', profile);
-}
-
-function getProfileData(profileID) {
-	if (profileID == "random") {
-		profileID = Math.floor(Math.random() * profiles.length) + 1;
-	} else {
-		profileID = parseInt(profileID);
-	}
-  	var profile = profiles[profileID-1]; // of by one, our first project has index 0
-  	return profile;	
-}
+exports.view = function(request, response){
+ response.render('profile', likes);
+};
