2d197760e6276ce01b5fe6b5e1bacb5c0929490d
Ruiqin Li <r5li@ucsd.edu>
2021-03-13 22:19:07 -0800

    zuowanle zaininmadejian

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,25 @@
-var likes = require('../like.json');
+var likes = require('../projects.json');
 
 exports.view = function(request, response){
- response.render('profile', likes);
+
+	// if (likes['projects'].length > 6) {
+	// 	temp = {
+	// 		projects:
+	// 		[]
+	// 	}
+	// 	temp.projects.push(likes['projects'].slice(6, ));
+	// 	response.render('profile', temp);
+	// }
+	temp = {
+		projects:[]
+	}
+	temp_list = likes['projects'].slice(6, likes['projects'].length)
+
+	for(var i = 0; i < temp_list.length; i++){
+		temp.projects.push(temp_list[i]);
+
+   }
+   console.log(temp)
+
+	response.render('profile', temp);
 };
